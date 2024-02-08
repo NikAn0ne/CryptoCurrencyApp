cc2a1593c9607d4928fd14c80afde649c2dd57ef
Nikita Aniperko <nikan0ne@mail.ru>
2024-02-08 21:24:54 +0300

    Added API dependency

---
+++ data/src/main/java/com/example/data/API/ApiInterface.kt
@@ -0,0 +1,12 @@
+package com.example.data.API
+
+
+import com.example.domain.model.MarketModel
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface ApiInterface {
+
+    @GET("data-api/v3/cryptocurrency/listing?start=1&limit=500")
+    suspend fun getMarketData(): Response<MarketModel>
+}
