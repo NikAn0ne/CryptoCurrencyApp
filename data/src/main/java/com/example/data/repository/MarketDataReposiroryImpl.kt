9d469874ccba9d05e2b01556271032e06a9232fc
Nikita Aniperko <nikan0ne@mail.ru>
2024-02-18 17:12:14 +0300

    MarketDataRepository with Implementation created

---
+++ data/src/main/java/com/example/data/repository/MarketDataReposiroryImpl.kt
@@ -0,0 +1,26 @@
+package com.example.data.repository
+
+import com.example.data.API.ApiInterface
+import com.example.data.API.ApiUtilities
+import com.example.data.util.toDomain
+import com.example.domain.model.CryptoCurrency
+import com.example.domain.repository.MarketDataRepository
+import java.lang.RuntimeException
+
+class MarketDataReposiroryImpl(private val api: ApiInterface): MarketDataRepository {
+    override suspend fun getMarketData(): MutableList<CryptoCurrency>? {
+        return runCatching {
+            val res = api.getMarketData()
+            if (res.isSuccessful){
+                res.body()?.map {
+                    it.toDomain()
+                }?.toMutableList()
+            }
+            else {
+                throw RuntimeException("Sorry, there is a problem to get the data")
+            }
+        }.getOrElse{
+            return null
+        }
+    }
+}
