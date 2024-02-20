71371ca2cda1308591ed8012e39ebe3174987a03
Nikita Aniperko <nikan0ne@mail.ru>
2024-02-20 18:14:53 +0300

    ViewModel dependencies added

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -43,26 +43,43 @@ dependencies {
     implementation(project(":data"))
     implementation(project(":domain"))
 
+    val lifecycle_version = "2.7.0"
 
+    //SmoothBottomBar
     implementation ("com.github.ibrahimsn98:SmoothBottomBar:1.7.9")
 
+    //SpinKit
     implementation ("com.github.ybq:Android-SpinKit:1.4.0")
 
+    //Default
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+
+    //Espresso
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    //Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    //Glide
     implementation ("com.github.bumptech.glide:glide:4.16.0")
 
+    //ViewPager2
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
+
+    //ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+
+    //LifeData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
 
 }
