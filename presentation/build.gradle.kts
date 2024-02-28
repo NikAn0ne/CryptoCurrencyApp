296362cf1b7991f980de2f387eb8ea69c7c23cd0
Nikita Aniperko <nikan0ne@mail.ru>
2024-02-28 14:23:38 +0300

    SplashScreen successfully created

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -82,4 +82,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
 
+    //Splash API
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
 }
--- presentation/src/main/java/com/example/cryptocurrencyapp/MainActivity.kt
+++ presentation/src/main/java/com/example/cryptocurrencyapp/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.cryptocurrencyapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.PopupMenu
+import android.window.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.findNavController
 import com.example.cryptocurrencyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().setKeepOnScreenCondition{
+            false
+        }
+
         super.onCreate(savedInstanceState)
         binding =  ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
